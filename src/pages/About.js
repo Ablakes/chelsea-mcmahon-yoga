860c9f01d391cd573d9a73a02e91223c42670a7f
Ablakes <blakesley.alex@gmail.com>
2018-08-23 15:51:38 -0700

    Second initial commit :)

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>About something</p>
+    <Link to="/">Go to home</Link>
+  </div>
+)
+
+export default About
